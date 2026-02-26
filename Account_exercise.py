9e529352f8b6bed9a7e95fd1155e8c83d71dceb2
fabiosuniga <fabiosuniga@hotmail.com>
2026-02-25 23:36:48 -0300

    Adicionando exercício de conta bancária

--- Account_exercise.py
+++ Account_exercise.py
@@ -1,2 +1,25 @@
 class Account:
-    pass
+
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def __str__(self):
+        return "Owner name: " + self.owner + ", Balance: $" + str(self.balance)
+
+    def withdraw(self, value):
+        if self.balance > value:
+            self.balance -= value
+            print("Withdrawal Accepted")
+        else: print("Insuficient balance for this operation!")
+
+    def deposit(self, value):
+        self.balance += value
+        print("Deposit Accepted")
+
+acct1 = Account('Jose',100)
+print(acct1)
+acct1.deposit(50)
+acct1.withdraw(75)
+acct1.withdraw(500)
+
